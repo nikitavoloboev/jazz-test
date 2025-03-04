5815edee2c9a29fa7725f755e78992359cadb125
Nikita <github@nikiv.dev>
2025-03-04 15:10:34 +0100

    .

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -13,22 +13,20 @@ function App() {
       <header>
         <nav className="container flex justify-between items-center py-3">
           {isAuthenticated ? (
-            <span>You're logged in.</span>
+            <span></span>
           ) : (
-            <span>Authenticate to share the data with another device.</span>
+            <span>Auth to share data with another device</span>
           )}
           <AuthButton />
         </nav>
       </header>
       <main className="container mt-16 flex flex-col gap-8">
         <Logo />
-
         <div className="text-center">
           <pre className="text-left inline-block">
             {JSON.stringify(me?.root, null, 2)}
           </pre>
         </div>
-
         <div className="text-center">
           <h1>
             Welcome{me?.profile.firstName ? <>, {me?.profile.firstName}</> : ""}
